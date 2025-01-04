25f62c8130a8374ebed32b05f6ae6f67bc136f5f
Kevin Zimmerman <4733573+kczimm@users.noreply.github.com>
2025-01-03 19:59:28 -0600

    rm test rosen

--- tests/test_rosen.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page, expect
-
-from vehicles.rosen import get_vins
-
-
-def test_vehicle_information(page: Page):
-    page.set_extra_http_headers({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
-    vins = get_vins(page)
-    assert len(vins) > 0
